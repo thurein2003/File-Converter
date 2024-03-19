673035f3110fcc442ec934669acbacf7c2891733
YuNandar-Kyaw <yunandarkyaw677@gmail.com>
2024-03-19 12:14:36 +0630

    sdf

---
+++ FileConverter/FileConverter/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.ForeColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
